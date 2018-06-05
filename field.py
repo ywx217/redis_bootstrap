5ee83f6482c0dd7804abb63463da0801e20fe8f0
杨文轩 <gzyangwenxuan@corp.netease.com>
2018-06-05 21:01:06 +0800

    container not finished

--- field.py
+++ field.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 __author__ = 'ywx217@gmail.com'
+import collections
 
 DEFAULT_PLACEHOLDER = object()
 
@@ -55,3 +56,65 @@ class FloatField(PrimitiveField):
 class StrField(PrimitiveField):
 	def _convert(self, val):
 		return str(val)
+
+
+class ContainerField(Field):
+	# container field represents a set of objects
+	def __init__(self, redis_key, var_type, default_factory):
+		super(ContainerField, self).__init__(redis_key)
+		self.var_type = var_type
+		assert callable(default_factory)
+		self.default_factory = default_factory
+
+	def _convert(self, val):
+		# todo: convert v -> var_type
+		return val
+
+	def load(self, mapping, obj):
+		# key format: <redis_key>.<index>[.<more_fields>]
+		container = self.default_factory()
+		prefix = self.redis_key + '.'
+		inner_mapping = collections.defaultdict(dict)
+		# {index: {field_name: val}}
+		for k, v in mapping.iteritems():
+			if not k.startswith(prefix):
+				continue
+			inner_k = k[len(prefix):]
+			splits = inner_k.split('.', 1)
+			if len(splits) == 2:
+				inner_mapping[splits[0]][splits[1]] = v
+			else:
+				inner_mapping[inner_k][''] = v
+
+		self._add_to(container, inner_mapping)
+		obj[self.var_name] = container
+
+	def save(self, save_map, obj):
+		# todo: save method to be implemented
+		pass
+
+	def _add_to(self, container, mapping):
+		raise NotImplementedError
+
+
+class ListField(ContainerField):
+	def __init__(self, redis_key, var_type):
+		super(ListField, self).__init__(redis_key, var_type, list)
+
+	def _add_to(self, container, mapping):
+		# {index: {field_name: val}}
+		for k, v in sorted(mapping.iteritems()):
+			i = int(k)
+			if i != len(container):
+				raise IndexError('index %d cannot add to list container of length %d', i, len(container))
+			container.append(self._convert(v))
+
+
+class MapField(ContainerField):
+	def __init__(self, redis_key, var_type):
+		super(MapField, self).__init__(redis_key, var_type, dict)
+
+	def _add_to(self, container, mapping):
+		# {index: {field_name: val}}
+		for k, v in mapping.iteritems():
+			container[k] = self._convert(v)
--- model.py
+++ model.py
@@ -50,19 +50,28 @@ class Model(dict):
 
 	def save(self, redis_context):
 		# save kv using hmset
+		save_map = self.save_mapping()
+		redis_context.hmset(self.__name, save_map)
+
+	def save_mapping(self):
 		if self.__readonly:
 			raise ValueError('model %s(%s) is read only!' % (self.__class__.__name__, self.__name))
 		save_map = {}
 		for field in self.__fields__:
 			field.save(save_map, self)
-		redis_context.hmset(self.__name, save_map)
+		return save_map
 
 	@classmethod
 	def load(cls, redis_context, index, read_only=False):
 		# load from redis using hgetall
 		name = cls.get_name(index)
 		m = redis_context.hgetall(name, watch=not read_only)
+		return cls.load_mapping(m, index, read_only)
+
+	@classmethod
+	def load_mapping(cls, mapping, index=None, read_only=False):
 		obj = cls(index, read_only)
 		for field in cls.__fields__:
-			field.load(m, obj)
+			field.load(mapping, obj)
 		return obj
+
